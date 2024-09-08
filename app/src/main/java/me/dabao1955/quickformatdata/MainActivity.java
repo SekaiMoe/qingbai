bbb81c51611d64f3fa4264b04732101e189492ee
SekaiMoe <153579552+SekaiMoe@users.noreply.github.com>
2024-09-08 11:20:05 +0000

    Init

---
+++ app/src/main/java/me/dabao1955/quickformatdata/MainActivity.java
@@ -0,0 +1,64 @@
+package me.dabao1955.quickformatdata;
+
+import android.app.*;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MainActivity extends Activity { 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("要留清白在人间");
+        builder.setMessage("点击确定键进行格式化 data 分区操作");
+
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String result1 = executeCommand("su -c /system/bin/mke2fs /data");
+				Toast.makeText(MainActivity.this, "手机将在5秒钟后重启！", Toast.LENGTH_SHORT).show();
+				String result2 = executeCommand("sleep 5s && su -c reboot"); 
+
+                /*AlertDialog.Builder resultDialog = new AlertDialog.Builder(MainActivity.this);
+                resultDialog.setTitle("命令执行结果");
+                resultDialog.setMessage(result1);
+
+                resultDialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                resultDialog.show();*/
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private String executeCommand(String command) {
+        StringBuilder output = new StringBuilder();
+        try {
+            Process process = Runtime.getRuntime().exec(command);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+            reader.close();
+
+            process.waitFor();
+        } catch (Exception e) {
+            output.append("命令执行失败: ").append(e.getMessage());
+        }
+        return output.toString();
+    }
+}
